b8bf8c853d4977517b34f42fa122b90409ad58ba
Arya Baladi <ariabaladi.ab@gmail.com>
2025-10-16 17:48:58 +0330

    Separate LoginView component

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { loginUser } from "@/api/authService"
 import { useAuth } from "@/context/AuthContext"
+import LoginView from "../../components/login/LoginView"
 
 export default function LoginPage() {
-    const router = useRouter();
     const { login } = useAuth();
   
     const [username, setUsername] = useState("");
@@ -30,39 +29,14 @@ export default function LoginPage() {
     };
   
     return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <form
-          onSubmit={handleSubmit}
-          className="w-full max-w-sm rounded-2xl bg-white p-6 shadow-md"
-        >
-          <h1 className="mb-4 text-2xl font-semibold text-center">Login</h1>
-  
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="mb-3 w-full rounded-lg border border-gray-300 p-2"
-          />
-  
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="mb-3 w-full rounded-lg border border-gray-300 p-2"
-          />
-  
-          {error && <p className="mb-3 text-sm text-red-600">{error}</p>}
-  
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full rounded-lg bg-blue-600 py-2 text-white hover:bg-blue-700 disabled:opacity-50"
-          >
-            {loading ? "Logging in..." : "Login"}
-          </button>
-        </form>
-      </div>
+      <LoginView
+      username={username}
+      password={password}
+      error={error}
+      loading={loading}
+      setUsername={setUsername}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+    />
     );
   }
---
+++ client/components/login/LoginView.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+interface LoginViewProps {
+  username: string;
+  password: string;
+  error: string;
+  loading: boolean;
+  setUsername: (value: string) => void;
+  setPassword: (value: string) => void;
+  handleSubmit: (e: React.FormEvent) => void;
+}
+
+export default function LoginView({
+  username,
+  password,
+  error,
+  loading,
+  setUsername,
+  setPassword,
+  handleSubmit,
+}: LoginViewProps) {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm rounded-2xl p-6 shadow-md"
+      >
+        <h1 className="mb-4 text-2xl font-semibold text-center">Login</h1>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="mb-3 w-full rounded-lg border border-gray-300 p-2"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="mb-3 w-full rounded-lg border border-gray-300 p-2"
+        />
+
+        {error && <p className="mb-3 text-sm text-red-600">{error}</p>}
+
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-full rounded-lg bg-white py-2 text-black hover:bg-gray-200 disabled:opacity-50"
+        >
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+}
